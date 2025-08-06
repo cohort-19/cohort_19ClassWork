2091c7ac3e5079cab757d0190496683d214a9802
benitah2901 <bukenyabenitah89@gmail.com>
2025-08-06 21:37:15 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,60 @@
+#assuming your talking to a boda guy with primary seven knowledge;briefly explain the following with examples;
+#a statement
+#a code
+#a memory address
+#a variable name
+#an operator
+#an operand
+#datatype
+#a list of values
+#a python script/module
+
+#A STATEMENT
+#is a single complete instruction to a computer.(eg salary=2000)
+print("no long talk")
+#this will tell the computer to say "no long talk"
+
+#A CODE
+#a code is a group of statements which is written to do something 
+#code is just like instructions,you write it to tell the computer what to do just like someone tells you where to go and what to carry
+print("i will save some money today to buy a helmet")
+print("i will stop working at 6pm")
+
+#A MEMORY ADDRESS
+#this is like a house address but here the computer keeps things there
+#for example if you have left your boda jacket at the stage in a box number 209,in computers that number is the memory address where data is stored.
+
+#A VARIABLE NAME
+#this is like a name used to store something in a computer,it shouldnt start with a number ,it shouldnt have spaces,it shouldnt start with capital letters as well.
+#its like a label you give to something so that the computer can remember it and use it later 
+#for example evening=200000
+
+#AN OPERATOR
+#a symbol or word or character that tells a computer what to do with a value. symbols include(+,-,*,/,//,%)
+#it has many categories which include mathematical.assignment,comparison,logical,and special
+#just like the road signs tel you what to do
+#for example if you made 20000 in the morning,30000 in the evening and you want to add them you say;
+print(20000+30000)
+#it will give you the total...you just have to keep changing the symbols to get the value you want
+
+#AN OPERAND
+#this is simply a value.
+# for example 150000+450000
+#150000 and 450000 are operands/values/numbers
+
+#DATATYPES
+#more like values to be stored in the memory of a computer,it shows the kind of value your working with
+#for example 500 is a number which is called integer in python
+#"benitah" is a text/string which is normally in quotes
+animals["dog","cat","goat"]
+#that is a list
+
+#A LIST OF VALUES
+#this is like a group of items you write down and is stored in square brackets separated by commas.
+my_fruits["apples","orange","mango","strawberries"]
+
+#A PYTHON SCRIPT/MODULE
+#a file of python code with an extension of .py
+#its like a guidebook or notebook where you write down alll the steps for something you want the computer to do
+
+
